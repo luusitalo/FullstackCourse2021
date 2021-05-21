03c957ebcd920c3ad14d50001b3c33f50a7e46fa
Uusitalo Laura <Laura.Uusitalo@env.fi>
2021-05-21 18:48:00 +0300

    Ex 2.12 done

--- Part2/countries/src/App.js
+++ Part2/countries/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
 const App = () => {
   const [ countries, setCountries ] = useState([ ])
   const [ searched, setSearched ] = useState('')
@@ -19,44 +18,73 @@ const App = () => {
   useEffect(hook, [])
   
   const handleSearched = (event) =>  {
+    console.log("Search value:", searched)
     event.preventDefault()
     setSearched(event.target.value)
   }
 
-   const matches = countries.filter(person => 
-    person.name.toLowerCase().includes(searched.toLowerCase()))
+  const matches = countries.filter(country =>
+        country.name.toLowerCase().includes(searched.toLowerCase())             
+  )
+  const matchLen = matches.length
+  console.log("matches:", matches)
+  console.log("matchLen:", matchLen)
 
-    const Countries = ({ countries }) => {
+
+  const Countries = ({ matches }) => {
+   
+    if (matches.length > 10) {
+      return <p>Too many matches to show.</p>
+    }
+
+    if (matches.length === 1) {
+      return <ShowCountry match = {matches[0]} />
+    }
+
+    else {
       return (
         <ul>
-        {countries.map(country => 
-          <Country key={ country.name } country={ country } />
-        )}
+          {matches.map(country => 
+            <li> {country.name} </li>
+          )}
         </ul>
-      )
-    }
+    )
+      }
+  
+  }
 
-    const Country = ({ country }) => {
-      //now returning only name, preparing to return other info too
-      return (
-        <li>{country.name}</li>
-      )
-    }
+  const ShowCountry = ({ match }) => {
+    return (
+      <div>
+        <h2> {match.name} </h2>
+        <p> Capital: {match.capital} </p>
+        <p>Population: {match.population} </p>    
+        <h3>Languages:</h3>
+          <ul>
+            {match.languages.map(language => 
+            <li> {language.name} </li>
+          )}
+          </ul>
+        <img src = {match.flag} alt = "flag" width = "100 %"  />  
+      </div>
+     )
+
+  }
 
 
   return (
     <div>
-      <h2>Country information</h2>
+      <h1>Country information</h1>
       <div>
         Find countries <input value={searched} onChange={handleSearched} />
       </div>
-      
-     <div>
-       <Countries countries = {matches} />
-     </div>
 
-    </div>
+      <div> 
+        <Countries matches = { matches } />
+      </ div>
+    </ div>    
   )
-}
+
+} // closes App
 
 export default App
